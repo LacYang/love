b32065e8613e64ba8123c76a369a05b1c0af8a9b
楊謹瑞 <LacYang@yangjinruideMacBook-Pro.local>
2014-03-02 23:55:41 +0800

    first-push

---
+++ app/controllers/love_controller.rb
@@ -0,0 +1,24 @@
+class LoveController < ApplicationController
+
+	def index
+		
+	end
+
+
+	def ourstory
+		
+	end
+
+	def list
+		
+	end
+
+	def homework
+		
+	end
+
+	def photo
+		
+	end
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Love::Application.config.secret_token = '3a7a2b133a14836128e4d6fc9277ff9e6d0f3d53207f8c61619706c5119303f196efc8aa9832e54781850e12abd6e088a9cacfff4bc56cef93bf535b073322e2'
